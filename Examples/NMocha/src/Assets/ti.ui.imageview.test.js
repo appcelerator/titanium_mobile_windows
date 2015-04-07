f7fdd39dd721fba379b82a6fc5890b4ebccbb583
Kota Iguchi <developer@infosia.co.jp>
2015-04-07 09:57:26 +0900

    Add unit test case for TIMOB-18684

--- Examples/NMocha/src/Assets/ti.ui.imageview.test.js
+++ Examples/NMocha/src/Assets/ti.ui.imageview.test.js
@@ -19,5 +19,34 @@ describe("Titanium.UI.ImageView", function() {
         should(label.image).eql('path/to/logo.png');
         should(label.getImage()).eql('path/to/logo.png');
         finish();
-    });
+	});
+
+    // TIMOB-18684
+	it("layoutWithSIZE_and_fixed", function (finish) {
+	    var win = Ti.UI.createWindow();
+	    var view = Ti.UI.createView({
+	        backgroundColor: "green",
+	        width: 100,
+	        height: Ti.UI.SIZE
+	    });
+	    var innerView = Ti.UI.createImageView({
+	        image: 'http://api.randomuser.me/portraits/women/0.jpg',
+	        width: 100,
+	        height: Ti.UI.SIZE,
+	        top: 0,
+	        left: 0
+	    });
+	    view.add(innerView);
+	    innerView.addEventListener("load", function (e) {
+	        should(innerView.size.height).eql(100);
+	        should(view.size.height).eql(innerView.size.height);
+	        should(view.size.width).eql(innerView.size.width);
+	        win.close();
+	        finish();
+	    });
+	    win.add(view);
+	    win.open();
+	});
+
+
 });
