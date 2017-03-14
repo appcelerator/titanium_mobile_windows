3acfe80acd879e7cada8510f1995a39ce107f2b0
Christopher Williams <chris.a.williams@gmail.com>
2017-03-14 15:51:54 -0400

    Skip another crashing/failing test

--- Examples/NMocha/src/Assets/ti.ui.layout.test.js
+++ Examples/NMocha/src/Assets/ti.ui.layout.test.js
@@ -1516,7 +1516,7 @@ describe('Titanium.UI.Layout', function () {
 	//
 	// left & right should just work for child view when parent is Window (vertical)
 	//
-	it('TIMOB-23372 #10', function (finish) {
+	(utilities.isWindows10() ? it.skip : it)('TIMOB-23372 #10', function (finish) {
 		var label = Ti.UI.createLabel({
 			left: 10,
 			right: 10,
