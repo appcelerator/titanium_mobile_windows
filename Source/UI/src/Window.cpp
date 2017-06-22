2a490ff38749d1a03d964f17d24ee6de3b6db7e3
Kota Iguchi <developer@infosia.co.jp>
2017-06-22 14:01:37 +0900

    [TIMOB-24873] Window.fullscreen doesn't work

--- Source/UI/src/Window.cpp
+++ Source/UI/src/Window.cpp
@@ -461,7 +461,16 @@ namespace TitaniumWindows
 			fullscreen ? statusBar->HideAsync() : statusBar->ShowAsync();
 #elif defined(IS_WINDOWS_10)
 			auto view = Windows::UI::ViewManagement::ApplicationView::GetForCurrentView();
-			fullscreen ? view->TryEnterFullScreenMode() : view->ExitFullScreenMode();
+			if (fullscreen) {
+				if (view->TryEnterFullScreenMode()) {
+					Windows::UI::ViewManagement::ApplicationView::PreferredLaunchWindowingMode = Windows::UI::ViewManagement::ApplicationViewWindowingMode::FullScreen;
+				} else {
+					TITANIUM_LOG_WARN("Unable to enter fullscreen mode");
+				}
+			} else {
+				view->ExitFullScreenMode();
+				Windows::UI::ViewManagement::ApplicationView::PreferredLaunchWindowingMode = Windows::UI::ViewManagement::ApplicationViewWindowingMode::Auto;
+			}
 #endif
 		}
 
