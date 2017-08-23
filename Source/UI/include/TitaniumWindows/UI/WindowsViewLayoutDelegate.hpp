c9142a74d1d4a397a951f68a0356bf686041b0b6
Kota Iguchi <developer@infosia.co.jp>
2017-08-23 09:03:33 +0900

    Fix: Style should be applied to underlying component

--- Source/UI/include/TitaniumWindows/UI/WindowsViewLayoutDelegate.hpp
+++ Source/UI/include/TitaniumWindows/UI/WindowsViewLayoutDelegate.hpp
@@ -475,8 +475,16 @@ namespace TitaniumWindows
 			virtual void setComponent(Windows::UI::Xaml::FrameworkElement^ component, Windows::UI::Xaml::Controls::Control^ underlying_control = nullptr, const bool& enableBorder = true);
 			virtual void setComponent(Windows::UI::Xaml::FrameworkElement^ component, Windows::UI::Xaml::Controls::Control^ underlying_control, Windows::UI::Xaml::Controls::Border^ border);
 
+			virtual void setStyleComponent(Windows::UI::Xaml::FrameworkElement^ styling_component) TITANIUM_NOEXCEPT
+			{
+				styling_component__ = styling_component;
+			}
+
 			virtual Windows::UI::Xaml::FrameworkElement^ getStyleComponent() const TITANIUM_NOEXCEPT
 			{
+				if (styling_component__) {
+					return styling_component__;
+				}
 				if (underlying_control__) {
 					return underlying_control__;
 				}
@@ -580,6 +588,7 @@ namespace TitaniumWindows
 			Windows::UI::Xaml::Controls::Border^ border__    { nullptr };
 			Windows::UI::Xaml::FrameworkElement^ component__ { nullptr };
 			Windows::UI::Xaml::Controls::Control^ underlying_control__ { nullptr };
+			Windows::UI::Xaml::FrameworkElement^ styling_component__{ nullptr };
 
 			Titanium::LayoutEngine::Node* layout_node__ { nullptr };
 
