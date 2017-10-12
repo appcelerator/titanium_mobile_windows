a51cf0c0a4c624f9ae50193695315fc1cfb53fec
Kota Iguchi <developer@infosia.co.jp>
2017-10-12 10:06:59 +0900

    exclude Windows Phone 8.1

--- Source/UI/src/ListView.cpp
+++ Source/UI/src/ListView.cpp
@@ -44,10 +44,12 @@ namespace TitaniumWindows
 			listview__->IsItemClickEnabled = true;
 			listview__->SelectionMode = Controls::ListViewSelectionMode::None;
 
+#if !defined(IS_WINDOWS_PHONE)
 			// TIMOB-25298: Remove default padding
 			auto itemStyle = ref new Windows::UI::Xaml::Style(Controls::ListViewItem::typeid);
 			itemStyle->Setters->Append(ref new Setter(Controls::ListViewItem::PaddingProperty, Windows::UI::Xaml::Thickness(0)));
 			listview__->ItemContainerStyle = itemStyle;
+#endif
 
 			// Since VisualTreeHelper is only available after Loaded event is fired, we need to register scroll/scrollend event after that.
 			loaded_event__ = listview__->Loaded += ref new RoutedEventHandler([this](Platform::Object^ sender, RoutedEventArgs^ e) {
