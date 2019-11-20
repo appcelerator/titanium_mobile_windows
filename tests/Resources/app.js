e0988f76e145c1664517782ada13b2316fd245f8
Kota Iguchi <developer@infosia.co.jp>
2019-11-20 09:57:12 +0900

    ci(jenkins): add node-compat module tests

--- tests/Resources/app.js
+++ tests/Resources/app.js
@@ -48,6 +48,14 @@ describe('__filename', function () {
 function loadTests() {
 	// ============================================================================
 	// Add the tests here using "require"
+	// node-compat core modules
+	require('./assert.test');
+	require('./buffer.test');
+	require('./fs.test');
+	require('./os.test');
+	require('./path.test');
+	require('./string_decoder.test');
+	require('./util.test');
 	// Titanium APIs
 	require('./ti.accelerometer.test');
 	require('./ti.analytics.test');
